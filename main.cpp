e4ca407387a36f90747bd416c6c18b6684a2f738
Joe <jtesta94@yahoo.com>
2017-03-29 21:39:00 -0400

    better main

--- main.cpp
+++ main.cpp
@@ -26,9 +26,10 @@ g++ -o main main.cpp -O2 -L/usr/X11R6/lib -lm -lpthread -lX11
 #include <iostream>
 #include <string>
 #include <stdio.h>
+#include <vector>
 using namespace cimg_library;
 
-int blurImage(CImg<unsigned char>& anImage,CImgDisplay& disp) {
+void blurImage(CImg<unsigned char>& anImage,CImgDisplay& disp) {
   double currentBlur = 1;
   CImg<unsigned char> secondImage = anImage;
   secondImage.blur(currentBlur);
@@ -41,7 +42,7 @@ int blurImage(CImg<unsigned char>& anImage,CImgDisplay& disp) {
     switch(blurChange) {
       case 0 :
         anImage = secondImage;
-        return 0;     
+        return;     
       case 1 : 
         if(currentBlur >= 0.5)
           currentBlur -= 0.5;
@@ -61,7 +62,106 @@ int blurImage(CImg<unsigned char>& anImage,CImgDisplay& disp) {
   }
 }
 
-void grayScaleAverage(CImg<unsigned char>& anImage,CImgDisplay& disp) {
+void brightDark(CImg<unsigned char>& anImage,CImgDisplay& disp) {
+  CImg<unsigned char> secondImage = anImage;
+  disp = secondImage;
+  int brightChange = 0;
+  int width = anImage.width();
+  int height = anImage.height();
+  while(1) {
+  	disp = secondImage;
+    std::cout << "press 1 to darken, 2 to brighten and 0 to go back" << std::endl;
+    std::cin >> brightChange;
+    switch(brightChange) {
+      case 0 :
+        anImage = secondImage;
+        return;     
+      case 1 : 
+        for (int h = 0; h < height; h++)
+        	for (int w = 0; w < width; w++) {
+	        	 secondImage(w,h,0,0) = secondImage(w,h,0,0)/2;
+                 secondImage(w,h,0,1) = secondImage(w,h,0,1)/2;
+                 secondImage(w,h,0,2) = secondImage(w,h,0,2)/2;
+	         }
+        break;
+      case 2 : 
+        for (int h = 0; h < height; h++)
+        	for (int w = 0; w < width; w++) {
+	        	 secondImage(w,h,0,0) = (secondImage(w,h,0,0)/2)+126;
+                 secondImage(w,h,0,1) = (secondImage(w,h,0,1)/2)+126;
+                 secondImage(w,h,0,2) = (secondImage(w,h,0,2)/2)+126;
+	         }
+        break;
+
+      default: 
+        std::cout << "Enter another value" << std::endl;
+        break;
+    }
+  }
+}
+
+void colorOverlay(CImg<unsigned char>& anImage) {
+  int colorChoice = 0;
+    std::cout << "Press 0 to go back\nPress 1 for red\nPress 2 for green"
+    "\nPress 3 for blue\nPress 4 for yellow\nPress 5 for magenta\nPress 6 for cyan" << std::endl;
+    std::cin >> colorChoice;
+    int width = anImage.width();
+    int height = anImage.height();
+    switch(colorChoice) {
+      case 0 :
+        return;    
+      case 1 :
+    	for (int h = 0; h < height; h++)
+        	for (int w = 0; w < width; w++) {
+	        	 //make red by changing R values to max
+	             anImage(w,h,0,0) = uint(255);
+	         }
+        break;
+      case 2 : 
+    	for (int h = 0; h < height; h++)
+        	for (int w = 0; w < width; w++) {
+	        	 //make green by changing G values to max
+	             anImage(w,h,0,1) = uint(255);
+	         }
+        break;
+       case 3 : 
+    	for (int h = 0; h < height; h++)
+        	for (int w = 0; w < width; w++) {
+	        	 //make blue by changing B values to max
+	             anImage(w,h,0,2) = uint(255);
+	         }
+        break;
+        case 4 : 
+    	for (int h = 0; h < height; h++)
+        	for (int w = 0; w < width; w++) {
+	        	 //make yellow by changing R and G values to max
+	             anImage(w,h,0,0) = uint(255);
+	             anImage(w,h,0,1) = uint(255);
+	         }
+        break;
+        case 5 : 
+    	for (int h = 0; h < height; h++)
+        	for (int w = 0; w < width; w++) {
+	        	 //make magenta by changing R and B values to max
+	             anImage(w,h,0,0) = uint(255);
+	             anImage(w,h,0,2) = uint(255);
+	         }
+        break;
+        case 6 : 
+    	for (int h = 0; h < height; h++)
+        	for (int w = 0; w < width; w++) {
+	        	 //make cyan by changing G and B values to max
+	             anImage(w,h,0,1) = uint(255);
+	             anImage(w,h,0,2) = uint(255);
+	         }
+        break;
+      default: 
+        std::cout << "Enter another value" << std::endl;
+        break;
+    }
+  }
+
+void grayScaleAverage(CImg<unsigned char>& anImage) {
     int width = anImage.width();
     int height = anImage.height();
     CImg<unsigned char> secondImage = anImage;
@@ -76,7 +176,7 @@ void grayScaleAverage(CImg<unsigned char>& anImage,CImgDisplay& disp) {
     anImage = secondImage;
 }
 
-void invert(CImg<unsigned char>& anImage,CImgDisplay& disp) {
+void invert(CImg<unsigned char>& anImage) {
     int width = anImage.width();
     int height = anImage.height();
     CImg<unsigned char> secondImage = anImage;
@@ -93,7 +193,8 @@ void invert(CImg<unsigned char>& anImage,CImgDisplay& disp) {
 
 int main() {
   CImg<unsigned char> baseImage("hongman.jpg");
-  CImg<unsigned char> lastImage("hongman.jpg");
+  //CImg<unsigned char> lastImage("hongman.jpg");
+  std::vector<CImg<unsigned char> > lastImage;
   CImg<unsigned char> currentImage("hongman.jpg");
   //const unsigned char red[] = { 255,0,0 }, green[] = { 0,255,0 }, blue[] = { 0,0,255 };
   //image.blur(2.5);
@@ -101,28 +202,48 @@ int main() {
   CImgDisplay main_disp(baseImage);
   CImgDisplay second_disp(currentImage);
   int choice = 0;
-  while(1) {
+
+  while(!main_disp.is_closed() && !second_disp.is_closed()) {
     second_disp = currentImage;
-    std::cout << "Press 0 to exit\nPress 1 to blur\nPress 2 to convert to grayscale\nPress 3 to invert(negative)" << std::endl;
+    std::cout << "Press 0 to exit\nPress 1 to undo\n"
+    "Press 2 to convert to grayscale\nPress 3 to invert(negative)"
+    "\nPress 4 to blur\nPress 5 to overlay a color\nPress 6 to change brightness" << std::endl;
     std::cin >> choice;
     switch(choice) {
     	case 0 : 
-        	std::cout << "Goodbye";
+        	std::cout << "Goodbye\n";
         	return 0;       
       	case 1 : 
-        	std::cout << '1';
-        	blurImage(currentImage,second_disp);
+      		if(lastImage.empty() == false){
+	      		currentImage = lastImage.back(); 
+	      		lastImage.pop_back();
+	      	}
+	      	else {
+	      		std::cout << "Nothing to undo\n" << std::endl;
+	      	}
         break;
         case 2 : 
-        	std::cout << '2';
-        	grayScaleAverage(currentImage,second_disp);
+        	lastImage.push_back(currentImage);
+        	grayScaleAverage(currentImage);
         break;
-        case 3 : 
-        	std::cout << '3';
-        	invert(currentImage,second_disp);
+        case 3 :
+        	lastImage.push_back(currentImage);
+        	invert(currentImage);
         break;
+        case 4:
+      		lastImage.push_back(currentImage);
+        	blurImage(currentImage,second_disp);
+        break;
+        case 5:
+      		lastImage.push_back(currentImage);
+        	colorOverlay(currentImage);
+        break;
+        case 6:
+      		lastImage.push_back(currentImage);
+        	brightDark(currentImage,second_disp);
+        break;	
       	default: 
-        	std::cout << "Enter another value" << std::endl;
+        	std::cout << "Enter another value\n" << std::endl;
         break;
     }
   }
