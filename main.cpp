4dff2065d301f96a305a4f2dbeb0f0b95e724ddf
Joe <jtesta94@yahoo.com>
2017-04-06 14:35:04 -0400

    sharpen and blur and fixed brightness

--- main.cpp
+++ main.cpp
@@ -21,6 +21,8 @@ int main() {
 g++ -o main main.cpp -O2 -L/usr/X11R6/lib -lm -lpthread -lX11 
 #endif
 
+//Try if magnitude>10 then call the function again and push the last one onto the vector. pass the vector in function.
+
 
 
 #include "CImg.h"
@@ -68,38 +70,159 @@ void histogramEqualization(CImg<unsigned char>& anImage) {
 	anImage = secondImage;
 }
 
-void blurImage(CImg<unsigned char>& anImage,CImgDisplay& disp) {
-  double currentBlur = 1;
-  CImg<unsigned char> secondImage = anImage;
-  secondImage.blur(currentBlur);
-  disp = secondImage;
-  int blurChange = 0;
-  while(1) {
-  	disp = secondImage;
-    std::cout << "\nPress 0 to go back"
-    "\nPress 1 to decrease blur"
-    "\nPress 2 to increase blur" << std::endl;
-    std::cin >> blurChange;
-    switch(blurChange) {
-      case 0 :
-        anImage = secondImage;
-        return;     
-      case 1 : 
-        if(currentBlur >= 0.5)
-          currentBlur -= 0.5;
-        secondImage = anImage;
-        secondImage.blur(currentBlur);
+int sharpen(CImg<unsigned char>& anImage,CImgDisplay& disp,bool recursive = false,int choice = 3) {
+	int width = anImage.width();
+  	int height = anImage.height();
+	int maskSize = 5;
+	int magnitude = 0;
+	CImg<unsigned char> secondImage = anImage;
+	int mask[maskSize][maskSize] = {{-1,-1,-1,-1,-1},{-1,-1,-1,-1,-1},{-1,-1,25,-1,-1},{-1,-1,-1,-1,-1},{-1,-1,-1,-1,-1}};
+	//int mask[maskSize][maskSize] = {{1,1,1,1,1},{1,1,1,1,1},{1,1,1,1,1},{1,1,1,1,1},{1,1,1,1,1}};
+	//int mask[maskSize][maskSize] = {{0,-1,0},{-1,5,-1},{0,-1,0}};
+	int edgeSize = ((maskSize-1)/2);
+	int color = 0;
+	int magChange = choice;
+	float mag = 0;
+	while(1) {
+	  	disp = secondImage;
+	    switch(magChange) {
+	      case 0 :
+	        anImage = secondImage;
+	        return 5;     
+	      case 1 : 
+	        if(magnitude > 0)
+	          magnitude -= 1;
+          else if(recursive == true){
+            anImage = secondImage;
+            return 1;
+          }
+	        break;
+	      case 2 : 
+	      	if(magnitude < 5)
+	        	magnitude += 1;
+          else if (sharpen(secondImage,disp,true,2) == 5) {
+            anImage = secondImage;
+            return 5;
+          }
+	        break;
+        case 3:
         break;
-      case 2 : 
-        currentBlur += 0.5;
-        secondImage = anImage;
-        secondImage.blur(currentBlur);
-        break;
+	      default: 
+	        std::cout << "Enter another value" << std::endl;
+	        break;
+		  }
+  		secondImage = anImage;
+  		mag = float(magnitude)/5.0;
+  		int maskSum = 0;
+  		for (int i = 0; i < maskSize; i++)
+  			for (int j = 0; j < maskSize; j++)
+  				maskSum += mask[i][j];
+  		for (int color = 0; color < 3; color++) {
+  			for (int h=0; h<height; h++)
+  				for (int w = 0; w < width; w++){
+  					secondImage(w,h,0,color)=anImage(w,h,0,color);
+  				}
+  			for (int h = edgeSize; h < height-edgeSize; h++){
+  				for (int w = ((maskSize-1)/2); w < width - ((maskSize-1)/2); w++){
+  					int sum1=0;
+  					for (int l = 0; l < maskSize; l++)
+  						for (int m = 0; m < maskSize; m++){
+  							sum1 += anImage(w+l-edgeSize,h+m-edgeSize,0,color)*mask[l][m];
+  						}	
+  					int value = ((1.0-mag)*anImage(w,h,0,color)) + (mag*int(float(sum1)/maskSum));
+  					//int value = int(float(sum1)/maskSum);
+  					if ((value <= 255) && (value >= 0))
+  						secondImage(w,h,0,color)= value;
+  					else if(value > 255)
+  						secondImage(w,h,0,color) = 255;
+  					else
+  						secondImage(w,h,0,color) = 0;
+  				}
+  			}
+		  }
+      disp = secondImage;
+      std::cout << "\nPress 0 to go back"
+      "\nPress 1 to decrease sharpen"
+      "\nPress 2 to increase sharpen" << std::endl;
+      std::cin >> magChange;
+	}
+}
 
-      default: 
-        std::cout << "Enter another value" << std::endl;
+int blurImage(CImg<unsigned char>& anImage,CImgDisplay& disp,bool recursive = false,int choice = 3) {
+  int width = anImage.width();
+  int height = anImage.height();
+  int maskSize = 5;
+  int magnitude = 0;
+  CImg<unsigned char> secondImage = anImage;
+  //int mask[maskSize][maskSize] = {{-1,-1,-1,-1,-1},{-1,-1,-1,-1,-1},{-1,-1,25,-1,-1},{-1,-1,-1,-1,-1},{-1,-1,-1,-1,-1}};
+  int mask[maskSize][maskSize] = {{1,1,1,1,1},{1,1,1,1,1},{1,1,1,1,1},{1,1,1,1,1},{1,1,1,1,1}};
+  //int mask[maskSize][maskSize] = {{0,-1,0},{-1,5,-1},{0,-1,0}};
+  int edgeSize = ((maskSize-1)/2);
+  int color = 0;
+  int magChange = choice;
+  float mag = 0;
+  while(1) {
+      disp = secondImage;
+      switch(magChange) {
+        case 0 :
+          anImage = secondImage;
+          return 5;     
+        case 1 : 
+          if(magnitude > 0)
+            magnitude -= 1;
+          else if(recursive == true){
+            anImage = secondImage;
+            return 1;
+          }
+          break;
+        case 2 : 
+          if(magnitude < 2)
+            magnitude += 1;
+          else if (blurImage(secondImage,disp,true,2) == 5) {
+            anImage = secondImage;
+            return 5;
+          }
+          break;
+        case 3:
         break;
-    }
+        default: 
+          std::cout << "Enter another value" << std::endl;
+          break;
+      }
+      secondImage = anImage;
+      mag = float(magnitude)/2.0;
+      int maskSum = 0;
+      for (int i = 0; i < maskSize; i++)
+        for (int j = 0; j < maskSize; j++)
+          maskSum += mask[i][j];
+      for (int color = 0; color < 3; color++) {
+        for (int h=0; h<height; h++)
+          for (int w = 0; w < width; w++){
+            secondImage(w,h,0,color)=anImage(w,h,0,color);
+          }
+        for (int h = edgeSize; h < height-edgeSize; h++){
+          for (int w = ((maskSize-1)/2); w < width - ((maskSize-1)/2); w++){
+            int sum1=0;
+            for (int l = 0; l < maskSize; l++)
+              for (int m = 0; m < maskSize; m++){
+                sum1 += anImage(w+l-edgeSize,h+m-edgeSize,0,color)*mask[l][m];
+              } 
+            int value = ((1.0-mag)*anImage(w,h,0,color)) + (mag*int(float(sum1)/maskSum));
+            //int value = int(float(sum1)/maskSum);
+            if ((value <= 255) && (value >= 0))
+              secondImage(w,h,0,color)= value;
+            else if(value > 255)
+              secondImage(w,h,0,color) = 255;
+            else
+              secondImage(w,h,0,color) = 0;
+          }
+        }
+      }
+      disp = secondImage;
+      std::cout << "\nPress 0 to go back"
+      "\nPress 1 to decrease blur"
+      "\nPress 2 to increase blur" << std::endl;
+      std::cin >> magChange;
   }
 }
 
@@ -140,6 +263,28 @@ void crop(CImg<unsigned char>& anImage,CImgDisplay& disp) {
   }
 }
 
+void blurOrSharpen(CImg<unsigned char>& anImage,CImgDisplay& disp) {
+    std::cout << "\nPress 0 to go back"
+    "\nPress 1 to blur"
+    "\nPress 2 to sharpen" << std::endl;
+    int choice;
+    std::cin >> choice;
+    switch(choice) {
+      case 0:
+        return;
+        break;
+      case 1:
+        blurImage(anImage,disp);
+        break;
+      case 2:
+        sharpen(anImage,disp);
+        break;
+      default:
+        std::cout << "Enter another value" << std::endl;
+        break;
+    }
+}
+
 void brightDark(CImg<unsigned char>& anImage,CImgDisplay& disp) {
   CImg<unsigned char> secondImage = anImage;
   disp = secondImage;
@@ -152,25 +297,41 @@ void brightDark(CImg<unsigned char>& anImage,CImgDisplay& disp) {
     "\nPress 1 to darken"
     "\nPress 2 to brighten" << std::endl;
     std::cin >> brightChange;
+    int value = 0;
     switch(brightChange) {
       case 0 :
         anImage = secondImage;
         return;     
       case 1 : 
-        for (int h = 0; h < height; h++)
-        	for (int w = 0; w < width; w++) {
-	        	 secondImage(w,h,0,0) = secondImage(w,h,0,0)/2;
-                 secondImage(w,h,0,1) = secondImage(w,h,0,1)/2;
-                 secondImage(w,h,0,2) = secondImage(w,h,0,2)/2;
-	         }
+        for (int color = 0; color < 3; color++) {
+          for (int h = 0; h < height; h++)
+          	for (int w = 0; w < width; w++) {
+  	        	    value = secondImage(w,h,0,color)/1.1;
+                  if ((value <= 255) && (value >= 0))
+                    secondImage(w,h,0,color)= value;
+                  else if(value > 255)
+                    secondImage(w,h,0,color) = 255;
+                  else
+                    secondImage(w,h,0,color) = 0;
+            }
+  	     }
         break;
       case 2 : 
-        for (int h = 0; h < height; h++)
-        	for (int w = 0; w < width; w++) {
-	        	 secondImage(w,h,0,0) = (secondImage(w,h,0,0)/2)+126;
-                 secondImage(w,h,0,1) = (secondImage(w,h,0,1)/2)+126;
-                 secondImage(w,h,0,2) = (secondImage(w,h,0,2)/2)+126;
-	         }
+        for (int color = 0; color < 3; color++) {
+          for (int h = 0; h < height; h++)
+          	for (int w = 0; w < width; w++) {
+  	        	 value = (secondImage(w,h,0,color)/1.1)+23;
+               if ((value <= 255) && (value >= 0))
+                secondImage(w,h,0,color)= value;
+              else if(value > 255)
+                secondImage(w,h,0,color) = 255;
+              else
+                secondImage(w,h,0,color) = 0;
+            }
+  	     }
+        
+
+
         break;
 
       default: 
@@ -367,10 +528,10 @@ void invert(CImg<unsigned char>& anImage) {
 
 
 int main() {
-  CImg<unsigned char> baseImage("hongman.jpg");
+  CImg<unsigned char> baseImage("len_full.jpg");
   //CImg<unsigned char> lastImage("hongman.jpg");
   std::vector<CImg<unsigned char> > lastImage;
-  CImg<unsigned char> currentImage("hongman.jpg");
+  CImg<unsigned char> currentImage("len_full.jpg");
   //const unsigned char red[] = { 255,0,0 }, green[] = { 0,255,0 }, blue[] = { 0,0,255 };
   //image.blur(2.5);
   //blurImage(image);
@@ -384,12 +545,12 @@ int main() {
     "\nPress 1 to undo"
     "\nPress 2 to convert to grayscale"
     "\nPress 3 to invert(negative)"
-    "\nPress 4 to blur"
+    "\nPress 4 to blur or sharpen"
     "\nPress 5 to overlay a color"
     "\nPress 6 to change brightness"
     "\nPress 7 to add noise"
     "\nPress 8 to crop" 
-    "\nPress 9 to equalize histogram"
+    "\nPress 9 to equalize the histogram"
     << std::endl;
     std::cin >> choice;
     switch(choice) {
@@ -416,7 +577,7 @@ int main() {
         break;
         case 4:
       		lastImage.push_back(currentImage);
-        	blurImage(currentImage,second_disp);
+        	blurOrSharpen(currentImage,second_disp);
         break;
         case 5:
       		lastImage.push_back(currentImage);
